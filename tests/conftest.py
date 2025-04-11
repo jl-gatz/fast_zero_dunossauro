f599833c98c1cd0c5ff033fafed90df532a96f49
JL Gatz <elan.rgb@gmail.com>
2025-04-11 15:31:53 -0300

    Adicionando teste user db com datetime, ex. 4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from fast_zero.models import table_registry
@@ -23,3 +26,23 @@ def session():
 
     # Após a session --> tear_down()
     table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+    def fake_time_handler(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_handler)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_handler)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fast_zero.models import User
@@ -17,3 +19,23 @@ def test_create_user_db(session):
     )
 
     assert result.username == 'dunossauro'
+
+
+def test_create_user_db_with_time(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'alice',
+        'password': 'secret',
+        'email': 'teste@test',
+        'created_at': time,
+        'updated_at': time,
+    }
