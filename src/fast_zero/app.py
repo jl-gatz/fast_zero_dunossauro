46f8fbb12cc8914e8fe3edfaec960089dc00b0d9
JL Gatz <elan.rgb@gmail.com>
2025-04-20 01:09:12 -0300

    Implementação de pw e auth
    Password e auth com JWT

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -2,12 +2,14 @@ from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema, Token
+from fast_zero.security import get_password_hash, verify_password
 
 app = FastAPI()
 
@@ -40,7 +42,9 @@ def create_user(
             )
 
     db_user = User(
-        username=user.username, email=user.email, password=user.password
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
     )
 
     session.add(db_user)
@@ -86,7 +90,7 @@ def update_users(
 
     db_user.username = user.username
     db_user.email = user.email
-    db_user.password = user.password
+    db_user.password = get_password_hash(user.password)
 
     session.add(db_user)
     session.commit()
@@ -108,6 +112,22 @@ def delete_user(user_id: int, session: Session = Depends(get_session)) -> dict:
     session.commit()
 
     return {'message': 'User deleted'}
+
+
+@app.post('/token', response_model=Token)
+def login_for_acess_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(
+        select(User).where(User.username == form_data.username)
+    )
+
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect username or password',
+        )
 
 
 @app.get('/pagina', status_code=HTTPStatus.OK, response_class=HTMLResponse)
---
+++ src/fast_zero/security.py
@@ -0,0 +1,18 @@
+from pwdlib import PasswordHash
+
+from jwt import encode
+
+
+pwd_context = PasswordHash.recommended()
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    pass
